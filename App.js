ba061bd0b0aee0b9a088bec6ca9d3f79755c7b4d
Khoi Dang <20041791.khoi@student.iuh.edu.vn>
2023-11-27 15:15:09 +0700

    Update menu icon

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import Clothing from "./screens/Clothing";
 import Product from "./screens/Product";
 import Header from "./screens/Header";
 import Footer from './screens/Footer';
-import Accessories from './screens/Accessories'
-import HomeLiving from './screens/HomeLiving'
+import Accessories from './screens/Accessories';
+import HomeLiving from './screens/HomeLiving';
 import SignIn from './screens/SignIn';
-import SignUp from './screens/SignUp'
-
+import SignUp from './screens/SignUp';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { faHouse, faMugHot, faShirt, faSocks, faStar } from '@fortawesome/free-solid-svg-icons/';
 
 export default App = () => {
   // const Stack = createNativeStackNavigator();
@@ -30,34 +31,46 @@ export default App = () => {
       <Drawer.Navigator initialRouteName="SignIn"
         screenOptions={({ navigation, route }) => ({
           headerShown: false,
-          drawerLabelStyle:{
+          drawerLabelStyle: {
             fontSize: 20,
             fontWeight: 'bold'
-          }
+          },
+
         })}
-        drawerContent={props => {
-          return (
-            <DrawerContentScrollView {...props}>
-              <DrawerItemList {...props}/>
-              {/* <DrawerItem label="Sign In" onPress={() => props.navigation.navigate("SignIn")} />
-              <DrawerItem label="Sign Up" onPress={() => props.navigation.navigate("SignUp")} /> */}
-            </DrawerContentScrollView>
-          )
-        }}
+      // drawerContent={props => {
+      //   return (
+      //     <DrawerContentScrollView {...props}>
+      //       <DrawerItemList {...props}/>
+      //     </DrawerContentScrollView>
+      //   )
+      // }}
       >
-      <Drawer.Screen name='Home' component={Index}></Drawer.Screen>
-      <Drawer.Screen name='Clothing' component={Clothing}></Drawer.Screen>
-      <Drawer.Screen name='Accessories' component={Accessories}></Drawer.Screen>
-      <Drawer.Screen name='Home Living' component={HomeLiving}></Drawer.Screen>
-      <Drawer.Screen name='Product' component={Product} options={{ drawerItemStyle: { display: 'none' } }}></Drawer.Screen>
-      <Drawer.Screen name='Header' component={Header} options={{ drawerItemStyle: { display: 'none' } }}></Drawer.Screen>
-      <Drawer.Screen name='Footer' component={Footer} options={{ drawerItemStyle: { display: 'none' } }}></Drawer.Screen>
-      <Drawer.Screen name='Sign In' component={SignIn} options={{   }}></Drawer.Screen>
-      <Drawer.Screen name='Sign Up' component={SignUp} options={{   }}></Drawer.Screen>
-      {/* <Drawer.Screen name='SignIn' component={SignIn} options={{ drawerItemStyle: { display: 'none' } }}></Drawer.Screen>
-      <Drawer.Screen name='SignUp' component={SignUp} options={{ drawerItemStyle: { display: 'none' } }}></Drawer.Screen> */}
-      
-    </Drawer.Navigator>
+        <Drawer.Screen name='Home' component={Index} options={{
+          drawerIcon: ({ focused, size }) => (
+            <FontAwesomeIcon icon={faHouse} size={25} color={focused ? '#00aff0' : 'grey'} />
+          )
+        }} />
+        <Drawer.Screen name='Clothing' component={Clothing} options={{
+          drawerIcon: ({ focused, size }) => (
+            <FontAwesomeIcon icon={faShirt} size={25} color={focused ? '#00aff0' : 'grey'} />
+          )
+        }} />
+        <Drawer.Screen name='Accessories' component={Accessories} options={{
+          drawerIcon: ({ focused, size }) => (
+            <FontAwesomeIcon icon={faSocks} size={25} color={focused ? '#00aff0' : 'grey'} />
+          )
+        }} />
+        <Drawer.Screen name='Home Living' component={HomeLiving} options={{
+          drawerIcon: ({ focused, size }) => (
+            <FontAwesomeIcon icon={faStar} size={25} color={focused ? '#00aff0' : 'grey'} />
+          )
+        }} />
+        <Drawer.Screen name='Sign In' component={SignIn} />
+        <Drawer.Screen name='Sign Up' component={SignUp} />
+        <Drawer.Screen name='Product' component={Product} options={{ drawerItemStyle: { display: 'none' } }} />
+        <Drawer.Screen name='Header' component={Header} options={{ drawerItemStyle: { display: 'none' } }} />
+        <Drawer.Screen name='Footer' component={Footer} options={{ drawerItemStyle: { display: 'none' } }} />
+      </Drawer.Navigator>
     </NavigationContainer >
   )
 }
